578fade892534d352d785561ecd5fb6f51034221
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-09 15:36:05 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _monthlycontroller = TextEditingController();
+  final TextEditingController _yearsController = TextEditingController();
+
+  double _totalSavings = 0.0;
+
+  void _calculate() {
+    final double monthly = double.tryParse(_monthlycontroller.text) ?? 0;
+    final int years = int.tryParse(_yearsController.text) ?? 0;
+
+    setState(() {
+      _totalSavings = monthly * 12 * years;
+    });
+  }
+  @override
+  Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(44.6),
-          child: TextField(
-            decoration: InputDecoration(labelText: 'Monthly saving'),
+        appBar: AppBar(title: const Text('Saving Simulator')),
+        body :Padding(
+          padding : const EdgeInsets.all(20),
+          child: Column(
+            children:[
+              TextField(
+                controller: _monthlycontroller,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration( labelText: 'Monthly Savings (\$)'),
+              ),
+              const SizedBox(height :12),
+              TextField(
+                controller: _yearsController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Number of Years'),
+              ),
+            ]
           ),
-        ),
-      ),
-    );
+        ),      
+      );
   }
 }
